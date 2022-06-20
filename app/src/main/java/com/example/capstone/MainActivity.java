398da901936b7c4c6ad6ebba5235ff1086f167da
Shirley Li <shirleysli@fb.com>
2022-06-20 16:40:27 -0700

    Implemented bottom navigation bar and skeleton UI for generate poem line page

--- app/src/main/java/com/example/capstone/MainActivity.java
+++ app/src/main/java/com/example/capstone/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.capstone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,51 +14,64 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.example.capstone.fragments.GenerateFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.elevation.SurfaceColors;
 import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    private View bLogout;
-    private EditText etUserInput;
-    private Button bGenerate;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Find the toolbar view inside the activity layout
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        // Sets the Toolbar to act as the ActionBar for this Activity window.
-        // Make sure the toolbar exists in the activity and is not null
-        setSupportActionBar(toolbar);
-
-        bLogout = findViewById(R.id.miLogout);
-        etUserInput = findViewById(R.id.etUserInput);
-        bGenerate = findViewById(R.id.bGenerate);
         // click generate button to take user input for a java class to do stuff to it
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+
+        // define your fragments here
+//        final Fragment fragment1 = new FeedFragment();
+        final Fragment fragment2 = new GenerateFragment();
+//        final Fragment fragment3 = new ThirdFragment();
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.action_feed:
+//                        fragment = fragment1;
+                        break;
+                    case R.id.action_generate:
+                        fragment = fragment2;
+                        break;
+                    case R.id.action_archive:
+//                        fragment = fragment3;
+                        break;
+                }
+                fragment = fragment2;
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+
+        bottomNavigationView.setSelectedItemId(R.id.action_generate);
+        bottomNavigationView.setBackgroundColor(SurfaceColors.SURFACE_2.getColor(this));
     }
 
-    // Menu icons are inflated just as they were with actionbar
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle presses on the action bar items
-        if (item.getItemId() == R.id.miLogout) {
-            ParseUser.logOutInBackground();
-            ParseUser currentUser = ParseUser.getCurrentUser(); // this will now be null
-            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-            startActivity(intent);
-            finish();
-        }
-        return super.onOptionsItemSelected(item);
-    }
+//        Logout code
+//        // Handle presses on the action bar items
+//        if (item.getItemId() == R.id.miLogout) {
+//            ParseUser.logOutInBackground();
+//            ParseUser currentUser = ParseUser.getCurrentUser(); // this will now be null
+//            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+//            startActivity(intent);
+//            finish();
+//        }
+//        return super.onOptionsItemSelected(item);
 
 
 }
