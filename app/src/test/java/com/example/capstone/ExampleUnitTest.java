143c4e5bd87ce8835838d6dafb475aa1ddf5f863
Shirley Li <shirleysli@fb.com>
2022-07-11 09:22:45 -0700

    Added OpenAI unit test! Creates an OpenAI thread to call the API with the prompt "test" and checks if the call returns a non-null object.

--- app/src/test/java/com/example/capstone/ExampleUnitTest.java
+++ app/src/test/java/com/example/capstone/ExampleUnitTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import com.example.capstone.models.OpenAIThread;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -11,7 +13,13 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    /**
+     * Tests OpenAI API call and on "test" prompt and checks if fetched value is null
+     */
+    public void openAI() throws InterruptedException {
+        OpenAIThread openAIThread = new OpenAIThread("test");
+        openAIThread.start();
+        openAIThread.join();
+        assertNotNull(openAIThread.getGeneratedLines());
     }
 }
